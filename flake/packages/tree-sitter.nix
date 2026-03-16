ce513fa3be21ed625e2162011f129d3e7af49748
Ruowen Qin <chinqrw@gmail.com>
2026-03-16 16:54:50 +0800

    cargohash update
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- flake/packages/tree-sitter.nix
+++ flake/packages/tree-sitter.nix
@@ -4,7 +4,7 @@
   neovim-dependencies,
 }:
 let
-  cargoHash = "sha256-u6RmwNR4QVwyuij5RlHTLC5lNNQpWMVrlQwfwF78pYc=";
+  cargoHash = "sha256-zh6KsnZ7s6VXGCggoYbLGeGnEZ7g7anjkz8C5/L4yXQ=";
 in
 pkgs.tree-sitter.overrideAttrs (oa: {
   src = neovim-dependencies.treesitter;
